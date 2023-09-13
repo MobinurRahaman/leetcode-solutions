9a572bee1af77d58a41a8de26486c709760731be
MobinurRahaman <mobinurrahaman@gmail.com>
2023-09-13 14:59:09 +0530

    create 12-integer-to-roman.js

---
+++ 12-integer-to-roman.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+
+var intToRoman = function (num) {
+  const hash = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+  
+  let result;
+  for (roman in hash) {
+    result += 
+  }
+};
+
+console.log(intToRoman(59))
